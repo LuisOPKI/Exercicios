92fe54a93f8e3656ff5e213759e0f0c23a69306b
luiso <luiso@LuisZoka>
2026-04-02 11:28:53 -0300

    Uso de metodo construtor ( e ponteiro de objetos THIS )

---
+++ ListaDeExercicio/src/atividadeaula/aula07/DataTeste.java
@@ -0,0 +1,12 @@
+package atividadeaula.aula07;
+
+public class DataTeste {
+	public static void main(String[] args) {
+		Data data1 = new Data(1, 1, 2011);
+		System.out.println(data1);
+		Data data2 = new Data(2, 2012);
+		System.out.println(data2);
+		Data data3 = new Data();
+		System.out.println(data3);
+	}
+}
